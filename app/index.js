5f0fe602dc4db40b4866e57de1da741accb514a4
rcdaflon <rcdaflon@outlook.com>
2020-11-25 20:14:33 -0300

    up

--- app/index.js
+++ app/index.js
@@ -1,28 +1,22 @@
 const PORTA = 3500;
-const express = require("express");
 const app = express();
-// View engine
-app.set('view engine', 'ejs');
-// Static
-app.use(express.static('public'));
+const express = require("express");
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 require("./routes/sexo.routes.js")(app);
 require("./routes/etnia.routes.js")(app);
-require("./routes/escola.routes.js")(app);
 require("./routes/renda.routes.js")(app);
-require("./routes/naturalidade.routes.js")(app);
+require("./routes/escola.routes.js")(app);
 require("./routes/etaria.routes.js")(app);
 require("./routes/matricula.routes.js")(app);
-
+require("./routes/naturalidade.routes.js")(app);
 
 app.get("/", (req, res) => {
     res.redirect("sexo");
 });
 
-
-//Rodando a APP
 app.listen(PORTA, () => {
-
     console.log(`APP Rodando: http://localhost:${PORTA}/`);
 });
